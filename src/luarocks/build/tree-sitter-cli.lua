ae1f287ff53bc47136370ab1bbee84e68487f2c5
FourierTransformer <FourierTransformer@users.noreply.github.com>
2025-12-22 19:26:45 -0600

    Improve gunzip extraction handling for Windows
    Add fallback for gunzip extraction on Windows

--- src/luarocks/build/tree-sitter-cli.lua
+++ src/luarocks/build/tree-sitter-cli.lua
@@ -63,6 +63,11 @@ so we can update the arch mappings accordingly at https://github.com/FourierTran
    util.printout("Extracting " .. filename)
    if platform == "windows" then
       ok, err = fs.gunzip(filename, "tree-sitter.exe")
+
+      -- sometimes the above works, sometimes it doesn't. This should allow both!
+      if not ok then
+         ok, err = fs.gunzip(filename)
+      end
    else
       ok, err = fs.gunzip(filename, "tree-sitter")
    end
